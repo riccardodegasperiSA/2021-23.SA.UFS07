47312070a235404fd454dcf1cfd25d7bbd1d5f7b
riccardodegasperiSA <riccardo.degasperi@itsrizzoli.it>
2022-06-10 12:44:47 +0200

    feat: added variables

--- design-system/react/src/atoms/input/Input.jsx
+++ design-system/react/src/atoms/input/Input.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 /**
  * Primary UI component for user interaction
  */
-export const Input = ({ isValid, onData, placeholder, type }) => {
+export const Input = ({ id, isValid, onData, placeholder, type, label }) => {
   function onInput(inputEvent) {
     console.log("input.onInput ", inputEvent);
     if (typeof(onData) === "function"){
@@ -15,26 +15,29 @@ export const Input = ({ isValid, onData, placeholder, type }) => {
   return (
     <>
       <input
+        id={id}
         onInput={onInput}
         placeholder={placeholder}
         type={type}
       >
       </input>
+      {label === undefined ? null : <label htmlFor={id}>{label}</label>}
       {isValid === undefined ? null : message}
     </>
   );
 };
 
 Input.propTypes = {
+  id: PropTypes.string.isRequired,
   isValid: PropTypes.bool,
+  label: PropTypes.string,
   onData: PropTypes.func,
-
   placeholder: PropTypes.string,
-
   type: PropTypes.string,
 };
 
 Input.defaultProps = {
   onData: undefined,
   type: "text",
+  label: undefined,
 };
--- design-system/react/stories/Input.stories.jsx
+++ design-system/react/stories/Input.stories.jsx
@@ -18,16 +18,40 @@ const Template = (args) => <Input {...args} />;
 export const InputText = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 InputText.args = {
+  id: "text",
   isValid: undefined,
   onData: undefined,
   placeholder: 'A placehonder text',
   type: 'text',
 };
 
-export const InputPsw = Template.bind({});
+export const InputPassword = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-InputPsw.args = {
+InputPassword.args = {
+  id: "pswd",
   isValid: undefined,
   onData: undefined,
+  placeholder: "A placeholder password",
   type: 'password',
+};
+
+export const InputEmail = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+InputEmail.args = {
+  id: "email",
+  isValid: undefined,
+  onData: undefined,
+  placeholder: "A placeholder email",
+  type: 'email',
+};
+
+export const InputRadio = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+InputRadio.args = {
+  id: "radio",
+  isValid: undefined,
+  onData: undefined,
+  placeholder: undefined,
+  type: 'radio',
+  label: "A radio example",
 };
